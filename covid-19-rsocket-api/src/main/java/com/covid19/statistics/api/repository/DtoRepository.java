feeccf8e7ed68e34a9c822f2fe578a5e1e943cc7
Roman Drohobytskyy <shatendri@gmail.com>
2020-04-22 16:03:47 +0300

    removed web flux version; reformatted pox.xml; removed reactiveMongoConfig; replaced long ID by UUID.

--- covid-19-rsocket-api/src/main/java/com/covid19/statistics/api/repository/DtoRepository.java
+++ covid-19-rsocket-api/src/main/java/com/covid19/statistics/api/repository/DtoRepository.java
@@ -1,8 +1,9 @@
 package com.covid19.statistics.api.repository;
 
 import com.covid19.statistics.api.dto.Dto;
+import java.util.UUID;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface DtoRepository extends ReactiveMongoRepository<Dto, Long> {
+public interface DtoRepository extends ReactiveMongoRepository<Dto, UUID> {
 
 }
