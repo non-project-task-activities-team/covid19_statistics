b4fc7b13d34cdb435ab752922d5fac12fb9ed58b
Mykhailo Yablon <yablon_michael@yahoo.com>
2020-04-23 12:10:14 +0300

    Initial kafka streaming implementation

---
+++ kafka-topology/src/main/java/com/demo/kafka/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.demo.kafka.controller;
+
+import com.demo.kafka.service.Producer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    public KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
