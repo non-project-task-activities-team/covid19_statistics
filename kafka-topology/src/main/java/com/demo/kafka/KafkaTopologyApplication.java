3498469a4f46629720b9195715468783e7687757
Mykhailo Yablon <Yablon_Michael@yahoo.com>
2020-05-20 14:05:45 +0300

    Aggregation function refactor

--- kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
+++ kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
@@ -32,34 +32,10 @@ public class KafkaTopologyApplication {
     }
 
     private static final String AMOUNT_STORE_NAME = "amount_store";
-    private Serde<Country> countrySerde;
-    private Serde<DailyStatistics> dailyStatisticsSerde;
-
-    public static class WordCountProcessorApplication {
-
-        public static final String INPUT_TOPIC = "countries";
-        public static final String OUTPUT_TOPIC = "output";
-        public static final int WINDOW_SIZE_MS = 30000;
-
-
-        @Bean
-        public Function<KStream<Bytes, String>, KStream<Bytes, WordCount>> process() {
-
-            return input -> input
-                    .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                    .map((key, value) -> new KeyValue<>(value, value))
-                    .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
-                    .windowedBy(TimeWindows.of(Duration.ofMillis(WINDOW_SIZE_MS)))
-                    .count(Materialized.as("WordCounts-1"))
-                    .toStream()
-                    .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()),
-							new Date(key.window().end()))));
-        }
-    }
 
     private KStream<String, AggregatedCountry> aggregate(KGroupedStream<String, Country> countries) {
         ObjectMapper objectMapper = new ObjectMapper();
-        dailyStatisticsSerde = new JsonSerde<>(DailyStatistics.class, objectMapper);
+        Serde<DailyStatistics> dailyStatisticsSerde = new JsonSerde<>(DailyStatistics.class, objectMapper);
         return countries.aggregate(this::initialize, this::aggregateAmount)
                 .toStream()
                 .map((key, value) -> new KeyValue<>(null,
@@ -83,70 +59,5 @@ public class KafkaTopologyApplication {
         return Materialized.<K, V>as(Stores.persistentKeyValueStore(storeName))
                 .withKeySerde(keySerde)
                 .withValueSerde(valueSerde);
-    }
-
-    static class WordCount {
-
-        private String word;
-
-        private long count;
-
-        private Date start;
-
-        private Date end;
-
-        @Override
-        public String toString() {
-            final StringBuffer sb = new StringBuffer("WordCount{");
-            sb.append("word='").append(word).append('\'');
-            sb.append(", count=").append(count);
-            sb.append(", start=").append(start);
-            sb.append(", end=").append(end);
-            sb.append('}');
-            return sb.toString();
-        }
-
-        WordCount() {
-
-        }
-
-        WordCount(String word, long count, Date start, Date end) {
-            this.word = word;
-            this.count = count;
-            this.start = start;
-            this.end = end;
-        }
-
-        public String getWord() {
-            return word;
-        }
-
-        public void setWord(String word) {
-            this.word = word;
-        }
-
-        public long getCount() {
-            return count;
-        }
-
-        public void setCount(long count) {
-            this.count = count;
-        }
-
-        public Date getStart() {
-            return start;
-        }
-
-        public void setStart(Date start) {
-            this.start = start;
-        }
-
-        public Date getEnd() {
-            return end;
-        }
-
-        public void setEnd(Date end) {
-            this.end = end;
-        }
     }
 }
