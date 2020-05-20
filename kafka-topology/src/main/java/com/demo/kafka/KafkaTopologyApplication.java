44cbeb0dea88185d2035b64da803c037316f1911
Mykhailo Yablon <Yablon_Michael@yahoo.com>
2020-05-20 15:01:20 +0300

    Aggregation function refactor

--- kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
+++ kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
@@ -6,15 +6,16 @@ import com.demo.kafka.models.DailyStatistics;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.KGroupedStream;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.Stores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.ArrayList;
@@ -24,11 +25,26 @@ import java.util.List;
 @SpringBootApplication
 public class KafkaTopologyApplication {
 
+    private static final String AMOUNT_STORE_NAME = "amount_store";
+
     public static void main(String[] args) {
         SpringApplication.run(KafkaTopologyApplication.class, args);
     }
 
-    private static final String AMOUNT_STORE_NAME = "amount_store";
+    @Bean
+    public KStream<String, AggregatedCountry> kStream(StreamsBuilder streamsBuilder) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonSerde<Country> countryJsonSerde = new JsonSerde<>(Country.class, objectMapper);
+        KGroupedStream<String, Country> countries = streamsBuilder.stream("countries", Consumed.with(Serdes.String(),
+                countryJsonSerde)).groupByKey(Grouped.with(Serdes.String(), countryJsonSerde));
+
+        KStream<String, AggregatedCountry> aggregatedCountries = aggregate(countries);
+
+        Produced<String, AggregatedCountry> produced = Produced.with(Serdes.String(), new JsonSerde<>(AggregatedCountry.class, objectMapper));
+        aggregatedCountries.to("counts", produced);
+        log.info("Aggregated countries", aggregatedCountries.toString());
+        return aggregatedCountries;
+    }
 
     private KStream<String, AggregatedCountry> aggregate(KGroupedStream<String, Country> countries) {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -40,7 +56,12 @@ public class KafkaTopologyApplication {
     }
 
     private List<DailyStatistics> aggregateAmount(String key, Country country, List<DailyStatistics> aggregatedAmount) {
-        aggregatedAmount.add(DailyStatistics.builder().build());
+        aggregatedAmount.add(DailyStatistics.builder()
+                .confirmed(country.getConfirmed())
+                .recovered(country.getRecovered())
+                .death(country.getDeath())
+                .build());
+        log.info("Inside aggregateAmount", aggregatedAmount.size());
         return aggregatedAmount;
     }
 
