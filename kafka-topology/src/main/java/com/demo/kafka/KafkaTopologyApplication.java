a0f6dd83e52c5fb54c9c2de88f562f409109e21b
Mykhailo Yablon <Yablon_Michael@yahoo.com>
2020-05-21 14:39:36 +0300

    Aggregation function refactor

--- kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
+++ kafka-topology/src/main/java/com/demo/kafka/KafkaTopologyApplication.java
@@ -9,20 +9,26 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.KGroupedStream;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.Stores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @SpringBootApplication
+@EnableBinding(KafkaTopologyApplication.KStreamProcessorWithBranches.class)
 public class KafkaTopologyApplication {
 
     private static final String AMOUNT_STORE_NAME = "amount_store";
@@ -31,24 +37,64 @@ public class KafkaTopologyApplication {
         SpringApplication.run(KafkaTopologyApplication.class, args);
     }
 
-    @Bean
-    public KStream<String, AggregatedCountry> kStream(StreamsBuilder streamsBuilder) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonSerde<Country> countryJsonSerde = new JsonSerde<>(Country.class, objectMapper);
-        KGroupedStream<String, Country> countries = streamsBuilder.stream("countries", Consumed.with(Serdes.String(),
-                countryJsonSerde)).groupByKey(Grouped.with(Serdes.String(), countryJsonSerde));
+    @StreamListener("countries")
+    @SendTo("counts")
+    public KStream<?, String> process(KStream<Object, Country> input) {
 
-        KStream<String, AggregatedCountry> aggregatedCountries = aggregate(countries);
+        return input
+                .groupBy((key, value) -> {
+                    log.info("Grouping By Country Code " + value.getCountryCode());
+                    return value.getCountryCode();
+                })
+                .aggregate(this::initialize, this::aggregateAmount,
+                        materializedAsPersistentStore("countries",
+                                Serdes.String(),
+                                new JsonSerde<>(List.class, new ObjectMapper()
+                                )))
+                .mapValues(value -> {
+                    log.info("TEST " + value.get(0).getConfirmed());
+                    return value.get(0).getConfirmed();
+                })
+                .toStream();
 
-        Produced<String, AggregatedCountry> produced = Produced.with(Serdes.String(), new JsonSerde<>(AggregatedCountry.class, objectMapper));
-        aggregatedCountries.to("counts", produced);
-        log.info("Aggregated countries", aggregatedCountries.toString());
-        return aggregatedCountries;
+//        return input
+//                .groupBy((key, value) -> value.getCountryCode())
+//                .aggregate(this::initialize, this::aggregateAmount)
+//                .mapValues(value -> {
+//                    log.info("TEST " + value.get(0).getConfirmed());
+//                    return value.get(0).getConfirmed();
+//                })
+//                .toStream();
     }
 
+    interface KStreamProcessorWithBranches {
+
+        @Input("countries")
+        KStream<?, ?> countries();
+
+        @Output("counts")
+        KStream<?, ?> counts();
+
+
+    }
+
+    //    @Bean
+//    public KStream<String, AggregatedCountry> kStream(StreamsBuilder streamsBuilder) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonSerde<Country> countryJsonSerde = new JsonSerde<>(Country.class, objectMapper);
+//        KGroupedStream<String, Country> countries = streamsBuilder.stream("countries", Consumed.with(Serdes.String(),
+//                countryJsonSerde)).groupByKey(Grouped.with(Serdes.String(), countryJsonSerde));
+//        log.info("Countries " + countries.count().toString());
+//
+//        KStream<String, AggregatedCountry> aggregatedCountries = aggregate(countries);
+//
+//        Produced<String, AggregatedCountry> produced = Produced.with(Serdes.String(), new JsonSerde<>(AggregatedCountry.class, objectMapper));
+//        aggregatedCountries.to("counts", produced);
+//        log.info("Aggregated countries   " + produced.toString());
+//        return aggregatedCountries;
+//    }
+//
     private KStream<String, AggregatedCountry> aggregate(KGroupedStream<String, Country> countries) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Serde<DailyStatistics> dailyStatisticsSerde = new JsonSerde<>(DailyStatistics.class, objectMapper);
         return countries.aggregate(this::initialize, this::aggregateAmount)
                 .toStream()
                 .map((key, value) -> new KeyValue<>(null,
@@ -61,14 +107,14 @@ public class KafkaTopologyApplication {
                 .recovered(country.getRecovered())
                 .death(country.getDeath())
                 .build());
-        log.info("Inside aggregateAmount", aggregatedAmount.size());
+        log.info("Inside aggregateAmount");
         return aggregatedAmount;
     }
 
     private List<DailyStatistics> initialize() {
         return new ArrayList<>();
     }
-
+//
     private <K, V> Materialized<K, V, KeyValueStore<Bytes, byte[]>> materializedAsPersistentStore(
             String storeName,
             Serde<K> keySerde,
