3abf69b62c2639a48c6648f7c101dd28bad41390
Roman Drohobytskyy <shatendri@gmail.com>
2020-06-17 17:26:56 +0300

    added a number input for the maxBackpresure field.

--- covid-19-react-client/src/date-picker.js
+++ covid-19-react-client/src/date-picker.js
@@ -30,32 +30,35 @@ class DatePicker extends Component {
         <div id="dateRangePickerContainer">
           <Form>
             <FormGroup>
-            <DateRangePicker
-              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-              startDateId="start_date_input" // PropTypes.string.isRequired,
-              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-              endDateId="end_date_input" // PropTypes.string.isRequired,
-              onDatesChange={({ startDate, endDate }) => {
-                this.btn.removeAttribute("disabled");
-                this.setState({ startDate, endDate });
-              }} // PropTypes.func.isRequired,
-              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-              isOutsideRange={(day) => isInclusivelyAfterDay(day, moment())}
-              initialVisibleMonth={() => moment().subtract(1, 'month')}
-              openDirection="down"
-              firstDayOfWeek={1}
-              displayFormat={"DD/MM/YYYY"}
-            />
-            <button 
-              type="button" 
-              className="btn btn-primary mb-2" 
-              id="dateRangeSubmitButton" 
-              onClick={this.handleSubmit.bind(this)}
-              ref={btn => { this.btn = btn; }}
-            >
-                Submit
-            </button>
+              <DateRangePicker
+                startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                startDateId="start_date_input" // PropTypes.string.isRequired,
+                endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                endDateId="end_date_input" // PropTypes.string.isRequired,
+                onDatesChange={({ startDate, endDate }) => {
+                  this.btn.removeAttribute("disabled");
+                  this.setState({ startDate, endDate });
+                }} // PropTypes.func.isRequired,
+                focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                isOutsideRange={(day) => isInclusivelyAfterDay(day, moment())}
+                initialVisibleMonth={() => moment().subtract(1, 'month')}
+                openDirection="down"
+                firstDayOfWeek={1}
+                displayFormat={"DD/MM/YYYY"}
+              />
+              <input className="btn btn-light mb-2" type="number" id="quantity" name="quantity"
+                pattern="\d*" min="1" max="1000000" placeholder="backpresure"
+              />
+              <button
+                type="button"
+                className="btn btn-primary mb-2"
+                id="dateRangeSubmitButton"
+                onClick={this.handleSubmit.bind(this)}
+                ref={btn => { this.btn = btn; }}
+              >
+                  Submit
+              </button>
             </FormGroup>
           </Form>
         </div>
